a625358e6e2416cf700f3a68e9d4d9b51241d2d1
Patryk Lotzwi <plotzwi@objectivity.co.uk>
2021-05-01 21:15:26 +0200

    Moved sso redirect to separate controller

--- backend/PlexSSO/Controllers/ChallengeController.cs
+++ backend/PlexSSO/Controllers/ChallengeController.cs
@@ -7,20 +7,26 @@ namespace PlexSSO.Controllers
 {
     [ApiController]
     [Route(Constants.ControllerPath)]
-    public class SsoController : CommonAuthController
+    public class ChallengeController : CommonAuthController
     {
         private readonly IAuthValidator _authValidator;
 
-        public SsoController(IAuthValidator authValidator)
+        public ChallengeController(IAuthValidator authValidator)
         {
             _authValidator = authValidator;
         }
 
         [HttpGet]
-        public SsoResponse Get()
+        public ActionResult<SsoResponse> Get()
         {
             var response = _authValidator.ValidateAuthenticationStatus(Identity, ServiceName, ServiceUri);
             Response.StatusCode = response.Status;
+
+            if (response.AccessBlocked)
+            {
+                return LocalRedirect($"/{ServiceName}");
+            }
+
             return response;
         }
     }
